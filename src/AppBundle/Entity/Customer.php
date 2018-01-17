24662ec76883263590eb7738a87aaac0ae11587c
kdu <kdu-prog@orange.fr>
2018-01-17 09:08:34 +0100

    changes entities

--- src/AppBundle/Entity/Customer.php
+++ src/AppBundle/Entity/Customer.php
@@ -1,15 +1,306 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kdu
- * Date: 15/01/2018
- * Time: 16:46
- */
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="customer")
+ */
 class Customer
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     */
+    private $name;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     */
+    private $email;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     */
+    private $password;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     */
+    private $address;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="town", type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     */
+    private $town;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="postcode", type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     */
+    private $postcode;
+
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\User", mappedBy="customer_id", cascade={"persist","remove"})
+     *
+     * @Assert\Valid()
+     */
+    private $users;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * Set name
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setfirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set gender
+     *
+     * @param string $gender
+     *
+     * @return User
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * Get gender
+     *
+     * @return string
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     *
+     * @return User
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set town
+     *
+     * @param string $town
+     *
+     * @return User
+     */
+    public function setTown($town)
+    {
+        $this->town = $town;
+
+        return $this;
+    }
+
+    /**
+     * Get town
+     *
+     * @return string
+     */
+    public function getTown()
+    {
+        return $this->town;
+    }
+    /**
+     * Set postcode
+     *
+     * @param string $postcode
+     *
+     * @return User
+     */
+    public function setPostcode($postcode)
+    {
+        $this->postcode = $postcode;
+
+        return $this;
+    }
+
+    /**
+     * Get postcode
+     *
+     * @return string
+     */
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+
+
+    public function setUsers($users)
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+    /**
+     * Add user
+     *
+     * @param \AppBundle\Entity\User $user
+     *
+     * @return Customer
+     */
+    public function addUser(\AppBundle\Entity\User $user)
+    {
+        $this->users[] = $user;
+        $user->setCustomer($this);
+
+        return $this;
+    }
+    /**
+     * Get users
+     *
+     * @return ArrayCollection
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
 }
--- src/AppBundle/Entity/Phone.php
+++ src/AppBundle/Entity/Phone.php
@@ -1,15 +1,287 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kdu
- * Date: 15/01/2018
- * Time: 16:59
- */
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="phone")
+ */
 class Phone
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="brand", type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     */
+    private $brand;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="model", type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     */
+    private $model;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="reference", type="string", length=25)
+     *
+     * @Assert\NotBlank()
+     */
+    private $reference;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="op_system", type="string", length=10)
+     */
+    private $opSystem;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="storage", type="integer")
+     *
+     * @Assert\NotBlank()
+     */
+    private $storage;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="color", type="string", length=25)
+     */
+    private $color;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     *
+     * @Assert\NotBlank(message="Veuillez décrire (même brièvement) le téléphone SVP.")
+     */
+    private $description;
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="string", length=255)
+     *
+     * @Assert\NotBlank()
+     */
+    private $price;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set brand
+     *
+     * @param string $brand
+     *
+     * @return Phone
+     */
+    public function setBrand($brand)
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    /**
+     * Get brand
+     *
+     * @return string
+     */
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    /**
+     * Set model
+     *
+     * @param string $model
+     *
+     * @return Phone
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
+     * Get model
+     *
+     * @return string
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Set reference
+     *
+     * @param string $reference
+     *
+     * @return Phone
+     */
+    public function setReference($reference)
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    /**
+     * Get reference
+     *
+     * @return string
+     */
+    public function getReference()
+    {
+        return $this->reference;
+    }
+
+
+    /**
+     * Set opSystem
+     *
+     * @param string $opSystem
+     *
+     * @return Phone
+     */
+    public function setOpSystem($opSystem)
+    {
+        $this->opSystem = $opSystem;
+
+        return $this;
+    }
+
+    /**
+     * Get opSystem
+     *
+     * @return string
+     */
+    public function getOpSystem()
+    {
+        return $this->opSystem;
+    }
+
+    /**
+     * Set storage
+     *
+     * @param string $storage
+     *
+     * @return Phone
+     */
+    public function setStorage($storage)
+    {
+        $this->storage = $storage;
+
+        return $this;
+    }
+
+    /**
+     * Get storage
+     *
+     * @return integer
+     */
+    public function getStorage()
+    {
+        return $this->storage;
+    }
+
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     *
+     * @return Phone
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get color
+     *
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Phone
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Phone
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
 }
